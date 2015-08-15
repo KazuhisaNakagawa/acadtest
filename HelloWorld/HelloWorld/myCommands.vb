0a7ef7f466c8f249f7e89ad5ff78f2d20531a175
KazuhisaNakagawa <kazuhisa.nakagawa@gray.plala.or.jp>
2015-08-15 13:06:30 +0900

    HelloWorld 作成
    HelloWorld を作成しました。

---
+++ HelloWorld/HelloWorld/myCommands.vb
@@ -0,0 +1,30 @@
+﻿' (C) Copyright 2011 by  
+'
+Imports System
+Imports Autodesk.AutoCAD.Runtime
+Imports Autodesk.AutoCAD.ApplicationServices
+Imports Autodesk.AutoCAD.DatabaseServices
+Imports Autodesk.AutoCAD.Geometry
+Imports Autodesk.AutoCAD.EditorInput
+
+' This line is not mandatory, but improves loading performances
+<Assembly: CommandClass(GetType(HelloWorld.MyCommands))> 
+Namespace HelloWorld
+
+    ' This class is instantiated by AutoCAD for each document when
+    ' a command is called by the user the first time in the context
+    ' of a given document. In other words, non static data in this class
+    ' is implicitly per-document!
+    Public Class MyCommands
+
+
+        <CommandMethod("HelloWorld", "HelloWorld", "HelloWorld", CommandFlags.Modal)> _
+        Public Sub HelloWorld()
+            MsgBox("HelloWorld!!")
+        End Sub
+
+
+
+    End Class
+
+End Namespace
